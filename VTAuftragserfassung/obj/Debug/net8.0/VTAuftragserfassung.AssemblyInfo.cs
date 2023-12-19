1c2a521517b1937fb23807aad48c64c9113740e1
Nils Reinshagen <nils.reinshagen@global-automotive-service.com>
2023-12-19 14:23:38 +0100

    styling

--- VTAuftragserfassung/obj/Debug/net8.0/VTAuftragserfassung.AssemblyInfo.cs
+++ VTAuftragserfassung/obj/Debug/net8.0/VTAuftragserfassung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VTAuftragserfassung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e34b98de81dd2a783536ff4c2d1e2723ccb5bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10945a3b31ea86ee634a9485507a42bb1cd94a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VTAuftragserfassung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VTAuftragserfassung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
