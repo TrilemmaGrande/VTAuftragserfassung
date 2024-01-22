bb7b197b9e14d1805ce57604de12aa98053d85aa
Nils Reinshagen <nils.reinshagen@global-automotive-service.com>
2024-01-22 10:13:22 +0100

    tabindex to customer form

--- VTAuftragserfassung/obj/Debug/net8.0/VTAuftragserfassung.AssemblyInfo.cs
+++ VTAuftragserfassung/obj/Debug/net8.0/VTAuftragserfassung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VTAuftragserfassung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a82305455e7a1c6a1e0ee9f72c31f05e76e274d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b271b6855b39abd883ce0ec4f9e0be419483f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VTAuftragserfassung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VTAuftragserfassung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
